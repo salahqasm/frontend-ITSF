3bff2155830964486117c651bef07803cd6cc008
salah <salahqasm@outlook.com>
2023-02-09 23:01:05 +0300

    student signup

--- src/Sign-page/Signup.js
+++ src/Sign-page/Signup.js
@@ -1,21 +1,42 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "./Signup.css";
 function Signup() {
+  const navigate = useNavigate();
  
   const [user, setUser] = useState("student");
- 
   function handleChange(e){
     console.log(e.target.value);
-    if(e.target.value=="student")
+    if(e.target.value==="student")
     {
       setUser("student");
     }else{
       setUser("Company");
     }
   }
+  async function submitHandler(e){
+    e.preventDefault();
+    const student={
+      fname:e.target.fname.value,
+      sname:e.target.sname.value,
+      lname:e.target.lname.value,
+      email:e.target.email.value,
+      password:e.target.password.value,
+      skill:e.target.skill.value
+    }
+    try{
+      await axios.post('http://localhost:3001/studentsignup',student);
+      navigate("/signin");
+
+    }catch(e){
+      console.log(e)
+      //strech goal
+    }
+  }
   return (
     <div className="Signup-main">
-      <form class="lform" action="#" method="post">
+      <form class="lform" onSubmit={(e)=>submitHandler(e)}>
 
         <h1 id="lgn">Sign Up</h1>
         <br/>
@@ -24,15 +45,16 @@ function Signup() {
       <input type="radio" name="user" id="company" value="company" onChange={(e)=>handleChange(e)}/><label for="company">Comapny</label>
         {/* <hr id="h-r" /> */}
     
-        <input type="text" placeholder="First Name" required />
-        <input type="text" placeholder="Second Name" required />
-        <input type="text" placeholder="Last Name" required />
-        <input type="email" placeholder="Enter Your E-mail" required />
+        <input type="text" id="fname" name="fname" placeholder="First Name" required />
+        <input type="text" id="sname" name="sname" placeholder="Second Name" required />
+        <input type="text" id="lname" name="lname" placeholder="Last Name" required />
+        <input type="text" id="skill" name="skill" placeholder="Skill" required />
+        <input type="email" id="email" name="email" placeholder="Enter Your E-mail" required />
 
-        <input id="pswrd2" type="password" placeholder="Creat Password" required />
-        <input id="pswrd" type="password" placeholder="Re-Enter Password" required />
+        <input id="pswrd2" name="password" type="password" placeholder="Creat Password" required />
+        <input id="pswrd" name="repassword" type="password" placeholder="Re-Enter Password" required />
         <input type="submit" value="Sign Up" />
-        {user=="student"?<h1>student</h1>:<h1>Comapny</h1>}
+        {user==="student"?<h1>student</h1>:<h1>Comapny</h1>}
 
       </form>
 

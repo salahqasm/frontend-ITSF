1ccb2a92b15d6b8ebeb52d93c7670ce2b088298a
salah <salahqasm@outlook.com>
2023-04-30 07:53:29 +0300

    task edited

--- src/Components/Student-components/Mytasks-components/MTask-component/MTask.js
+++ src/Components/Student-components/Mytasks-components/MTask-component/MTask.js
@@ -24,39 +24,40 @@ function MTask({ type, task }) {
             console.log(err);
         }
     }
-    return <div className="Mtask-task-main">
-        <div className="Mtask-task" >
-            <div style={{ float: "right" }}>
-                {type === "Request" &&
-                    <input type="button" value="Delete Request" className="delete-req" onClick={deleteReq} />
-                }
+    return <div className="Mtask-task" >
 
-
-            </div>
-            <h2>{task?.title}</h2>
-            <span><span>{task?.status}</span> - <span> Cost: {task?.credit} JD </span> - <span> Due Date: {task?.date}</span> </span>
-            <p>
-                {task?.description}
-            </p>
-            <div className="Mtask-required-skills">
-                {task?.skills?.map(elem =>
-                    <span>{elem.name}</span>
-                )}
-            </div>
-
-            {type === "InProcess" &&
-                <div className="Stask-submittion">
-                    <form >
-                        <label htmlFor="submission"><strong>Submission:</strong></label>
-                        <input id="submission" type="text" placeholder="Submittion URL" className="submission-input"
-                            value={submission && submission} onChange={(e) => setSubmission(e.target.value)} />
-                        <input type="submit" className="submission-button"
-                            value={task.submission === "" ? "Submit" : "Edit Submission"} />
-                    </form>
-                </div>
+        <div style={{ float: "right" }}>
+            {type === "Request" &&
+                <input type="button" value="Delete Request" className="delete-req" onClick={deleteReq} />
             }
         </div>
+
+        <h2>{task?.title}</h2>
+        <span><span><strong>Budget: </strong>{task?.credit} JD </span> - <span><strong>Due Date:</strong> {task?.date}</span> </span>
+        <p style={{ whiteSpace: "pre-wrap" }}>
+            <strong>Description:</strong>
+            <br />{task?.description}
+        </p>
+        <div className="Mtask-required-skills">
+            <strong>Required Skills: </strong>
+            {task?.skills?.map(elem =>
+                <span>{elem.name}</span>
+            )}
+        </div>
+
+        {type === "InProcess" &&
+            <div className="Stask-submittion">
+                <form >
+                    <label htmlFor="submission"><strong>Submission:</strong></label>
+                    <input id="submission" type="text" placeholder="Submittion URL" className="submission-input"
+                        value={submission && submission} onChange={(e) => setSubmission(e.target.value)} />
+                    <input type="submit" className="submission-button"
+                        value={task.submission === "" ? "Submit" : "Edit Submission"} />
+                </form>
+            </div>
+        }
     </div>
+
 }
 
 export default MTask;
